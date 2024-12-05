195f95dafc1ba714cc0bf3c637404d467d8b470c
jahidul39306 <jahidulislamananto@gmail.com>
2024-12-05 19:49:50 +0600

    sort by price complete

--- src/pages/AllEquipmentPage.jsx
+++ src/pages/AllEquipmentPage.jsx
@@ -1,56 +1,79 @@
 import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../provider/AuthProvider";
+import Loading from "../components/Loading";
 
 const AllEquipmentPage = () => {
     const { serverURL } = useContext(GlobalContext);
     const [defaultEquipments, setDefaultEquipments] = useState([]);
-
+    const [allEquipments, setAllEquipments] = useState([]);
+    const [loadingEqp, setLoadingEqp] = useState(true);
+    const [sortByPrice, setSortByPrice] = useState(false);
 
     const getAllEquipments = async () => {
         const res = await fetch(`${serverURL}/get-all-equipment`);
         const data = await res.json();
         setDefaultEquipments(data);
+        setAllEquipments([...data]);
+        setLoadingEqp(false);
     }
 
     useEffect(() => {
         getAllEquipments();
-    }, [])
+    }, []);
+
+    const handleSort = () => {
+        if (!sortByPrice) {
+            setAllEquipments(allEquipments.sort((a, b) => a.price - b.price));
+            setSortByPrice(!sortByPrice);
+        }
+        else {
+            setAllEquipments([...defaultEquipments]);
+            setSortByPrice(!sortByPrice);
+        }
+    }
+
+    if (loadingEqp) {
+        return <Loading></Loading>
+    }
 
     return (
-        <div className="overflow-x-auto max-w-3xl mx-auto mt-10">
-            <table className="table table-zebra">
-                {/* head */}
-                <thead>
-                    <tr>
-                        <th>#No.</th>
-                        <th>Item Name</th>
-                        <th>Category</th>
-                        <th>Price</th>
-                        <th>Available quantity</th>
-                        <th>Processing time</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        defaultEquipments.map((eq, idx) => {
-                            return (
-                                <tr key={eq._id}>
-                                    <th>{idx + 1}</th>
-                                    <td>{eq.itemName}</td>
-                                    <td>{eq.category}</td>
-                                    <td>{eq.price}</td>
-                                    <td>{eq.quantity}</td>
-                                    <td>{eq.processingTime} day</td>
-                                    <td>
-                                        <button className="btn btn-primary">Details</button>
-                                    </td>
-                                </tr>
-                            );
-                        })
-                    }
-                </tbody>
-            </table>
+        <div className="mt-10 container mx-auto text-center space-y-5">
+            <button onClick={handleSort} className="btn btn-primary">{sortByPrice ? "Sort by default" : "Sort by price"}</button>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra max-w-sm lg:max-w-full mx-auto">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#No.</th>
+                            <th>Item Name</th>
+                            <th>Category</th>
+                            <th>Price</th>
+                            <th>Available quantity</th>
+                            <th>Processing time</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allEquipments.map((eq, idx) => {
+                                return (
+                                    <tr key={eq._id}>
+                                        <th>{idx + 1}</th>
+                                        <td>{eq.itemName}</td>
+                                        <td>{eq.category}</td>
+                                        <td>{eq.price}</td>
+                                        <td>{eq.quantity}</td>
+                                        <td>{eq.processingTime} day</td>
+                                        <td>
+                                            <button className="btn btn-primary">Details</button>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
